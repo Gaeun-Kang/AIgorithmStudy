809c8ca027b0e18ff6385453206bd2dc6d3933d7
Gaeun-Kang <53822816+Gaeun-Kang@users.noreply.github.com>
2025-05-08 16:51:32 +0900

    Console Read-Write

---
+++ Introductory/C#IntroductoryProject/C#IntroductoryProject/ConsoleOutput.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_IntroductoryProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //기본 출력- 줄바꿈x, 줄바꿈o 
+            Console.Write("Hello");
+            Console.WriteLine("World!");
+
+            //콘솔 입력 및 변수 저장-출력 
+            Console.Write("안녕하세요! 이름을 입력해주세요");
+            string name = Console.ReadLine();
+            Console.Write(name + "님을 환영합니다!");
+        }
+    }
+}
