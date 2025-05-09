b15fbabf17b6fe47f36de25e817e1011a2fe0a8b
Gaeun-Kang <53822816+Gaeun-Kang@users.noreply.github.com>
2025-05-10 00:41:05 +0900

    ArithmeticOperators & DivideByZeroException

---
+++ Introductory/C#IntroductoryProject/C#IntroductoryProject/ArithmeticOperators.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+
+namespace C_IntroductoryProject
+{
+    internal class ArithmeticOperators
+    {
+        //산술연산자
+        //그냥 한 번씩 정리하기 
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("정수의 계산");
+            Console.WriteLine(123 + 45);
+            Console.WriteLine(123 - 45);
+            Console.WriteLine(123 * 45);
+            Console.WriteLine(123 / 45);
+            Console.WriteLine(123 % 45); //나머지
+
+
+            //DividByZeroException : 산술 연산자에서 나올 수 있는 에러
+            // 0으로 나눔예외
+          
+            
+            int x = 10, y = 0;
+            
+            Console.WriteLine(10.0 / y);
+            //Console.WriteLine(x/y);
+            
+            //실수를 0으로 나누기 때문에 처리됮 않은 예외가 출력된다.
+            //이 때 try-catch문을 사용하면 예외처리가 된다.
+
+            try
+            {
+                Console.WriteLine(x / y);
+
+            }
+            catch (Exception e) 
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+        }
+
+
+    }
+}
--- Introductory/C#IntroductoryProject/C#IntroductoryProject/obj/Debug/net5.0/C#IntroductoryProject.AssemblyInfo.cs
+++ Introductory/C#IntroductoryProject/C#IntroductoryProject/obj/Debug/net5.0/C#IntroductoryProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#IntroductoryProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+630e8c52ba7cd8c79f9e9a448d15444b6d9a9c05")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcb6d1080f0371a12f2e6de5f3bffe81796c3c95")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#IntroductoryProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#IntroductoryProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
